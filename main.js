a6a886d451c35a8e44f2f39d68214b9ef62b713e
Asa-kawa <kh7672ak@gmail.com>
2024-08-15 19:33:34 +0900

    変更

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-document.getElementById("button").addEventListener("click", () => {
+document.getElementById("description").addEventListener("click", () => {
   alert("ここには何もありません");
 });
 
-
-  document.getElementById("close").addEventListener("click", () => {
-    const result = confirm("本当に閉じていいですか");
-if (result) {
-  window.close();
-} else {
-  alert("キャンセルしました")
-}
+document.getElementById("close").addEventListener("click", () => {
+  const result = confirm("本当に閉じていいですか");
+  if (result) {
+    window.close();
+  } else {
+    alert("キャンセルしました")
+  }
 });
+
+document.getElementById("index").addEventListener("click", () => {
+  open("https://asa-kawa.github.io/Asa-kawaIndex/")
+});
+
+document.getElementById()
