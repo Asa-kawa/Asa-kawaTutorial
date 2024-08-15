45cc59317c751840b6aeefe5bc7258d5840f1432
Asa-kawa <kh7672ak@gmail.com>
2024-08-15 14:53:31 +0900

    変更

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 document.getElementById("button").addEventListener("click", () => {
   alert("ここには何もありません");
 });
+
+
+  document.getElementById("close").addEventListener("click", () => {
+    const result = confirm("本当に閉じていいですか");
+if (result) {
+  window.close();
+} else {
+  alert("キャンセルしました")
+}
+});
